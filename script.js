7737fd33ef9bdd7c6028ac0d4de03a84dc6431c9
dmuslima <pirmukhamedovamuslima01@gmail.com>
2025-03-14 14:31:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let info = prompt('String typedagi malumotni kiriting');
+console.log(typeof 'string');
+
+let number = prompt('Number typedagi malumotni kiriting');
+console.log(typeof 123);
+
+let boolean = prompt('Boolean typedagi malumotni kiriting');
+console.log(typeof true); 
+
+alert("hamma ma'lumotlarni to'g'ri kirgizganizga ishonchingiz komilmi?")
+
+alert(`Siz kiritgan ${info} so'zi string typega tegishli Siz kiritgan ${number} so'zi number typega tegishli Siz kiritgan ${boolean} so'zi number typega tegishli`)
+
